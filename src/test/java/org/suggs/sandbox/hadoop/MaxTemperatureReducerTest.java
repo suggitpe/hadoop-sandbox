4d011911e53ffa6c1588cb6534e72dd6ce814eb3
Peter Suggitt <me@suggs.org.uk>
2018-01-23 06:58:23 +0000

    updated reducer

---
+++ src/test/java/org/suggs/sandbox/hadoop/MaxTemperatureReducerTest.java
@@ -0,0 +1,22 @@
+package org.suggs.sandbox.hadoop;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.ReduceDriver;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MaxTemperatureReducerTest {
+
+    @Test
+    public void returnsMaximumIntegerFromValues() throws IOException {
+        new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+                .withReducer(new MaxTemperatureReducer())
+                .withInput(new Text("1950"), Arrays.asList(new IntWritable(10), new IntWritable(5), new IntWritable(9)))
+                .withOutput(new Text("1950"), new IntWritable(10))
+                .runTest();
+    }
+
+}
